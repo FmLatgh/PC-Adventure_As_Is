7da961cbc324c0c39455b3dae97f74d9eea0c363
Thinkworks <s.van.groenendaal@sintlucasedu.nl>
2023-03-22 15:50:11 +0100

    More changes in class

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 document.getElementById("mainTitle").innerText = "Point and click adventure";
+
+const gameWindow = document.getElementById("gameWindow");
+const mainCharacter = document.getElementById("mainCharacter");
+
+gameWindow.onclick = function() {
+    console.log("Clicked");
+}
