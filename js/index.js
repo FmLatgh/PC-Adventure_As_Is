be71f430e08ac74b29d529b7709d34c24617a00f
Thinkworks <s.van.groenendaal@sintlucasedu.nl>
2023-03-22 15:56:50 +0100

    Added localisation utilities

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ document.getElementById("mainTitle").innerText = "Point and click adventure";
 const gameWindow = document.getElementById("gameWindow");
 const mainCharacter = document.getElementById("mainCharacter");
 
-gameWindow.onclick = function() {
+gameWindow.onclick = function(e) {
     console.log("Clicked");
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left; //xpos in element
+    var y = e.clientY - rect.top; //ypos in element
+    console.log(x + " " + y);
+
 }
