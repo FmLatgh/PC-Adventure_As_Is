190e95c8936af6a8b851d055e069e2750edd212b
Thinkworks <s.van.groenendaal@sintlucasedu.nl>
2023-03-22 16:37:19 +0100

    We did it!! Last one today!!!

--- js/index.js
+++ js/index.js
@@ -9,7 +9,19 @@ gameWindow.onclick = function(e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left; //xpos in element
     var y = e.clientY - rect.top; //ypos in element
-    console.log(e.target.id);
+    
+    console.log(e.target.id); //Logs ID of click
+    switch(e.target.id) {
+        case "door1":
+            console.log("Door 1 clicked");
+            break;
+            case "door2":
+                console.log("Door 2 clicked");
+                break;
+                default:
+                    console.log("didn't click on something important!");
+                    break;
+    }
 
     mainCharacter.style.left = x - offsetCharacter +"px";
     mainCharacter.style.top = y - offsetCharacter +"px";
